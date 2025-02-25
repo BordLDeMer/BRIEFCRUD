e55f0ab7d556d9900c9f9d125aee592b3b1ea70d
benjamin.bidou <benjamin.bidou@campus-centre.fr>
2025-02-25 16:48:43 +0100

    yop

--- delete.php
+++ delete.php
@@ -39,6 +39,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
     <title>Ajouter un Produit</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
     <title>BRIEF CRUD</title>
 </head>
 <body>
